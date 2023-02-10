0215eaf5f245c9ce7a24d26ca4d4e8de76881ba8
Gabor Peresztegi <gabor.p1983@gmail.com>
2023-02-10 14:14:30 +0100

    Fix remove_unused_fonts.py, and add some new fonts

--- tools/remove_unused_fonts.py
+++ tools/remove_unused_fonts.py
@@ -11,36 +11,44 @@ Usage:
 
     [fonts_path]        Path of font files (example: "../fonts")
     [search_path]       Path to search for files containing font file names (example: "../")
+    [skip_directories]  Comma separated list of directories to skip (relative to "search_path")
 
 Script collects font file names, then searches for them in all ".py" and ".json" files.
 """
 
+SEARCH_FILE_EXTENSIONS = [".py", ".json"]
+
 try:
     fonts_dir = sys.argv[1]
     rootdir = sys.argv[2]
+    skip_directories = list(map(lambda d: os.path.join(rootdir, d), sys.argv[3].split(','))) if len(sys.argv) > 3 else []
 except:
     print(_USAGE)
     sys.exit(1)
 
-search_file_extensions = ["py", "json"]
 font_files = [name for name in os.listdir(fonts_dir) if name.endswith(".font")]
 used_font_files = []
 original_font_files_count = len(font_files)
 
 for currentdir, subdirs, files in os.walk(rootdir):
+    # Skip directories we are not interested in
+    if len([1 for skip_dir in skip_directories if skip_dir in currentdir]) > 0:
+        continue
+
     for file_name in files:
         # Skip files we are not interested in
-        parts = file_name.rsplit(".", 2)
-        if len(parts) < 2 or parts[1] not in search_file_extensions:
+        if len([1 for extension in SEARCH_FILE_EXTENSIONS if file_name.endswith(extension)]) == 0:
             continue
 
-        file_full_path = os.path.join(currentdir, file_name)
-        file_content = Path(file_full_path).read_text()
+        try:
+            file_full_path = os.path.join(currentdir, file_name)
+            file_content = Path(file_full_path).read_text()
 
-        # Check for font file usage:
-        for font_file in font_files:
-            if font_file in file_content:
-                used_font_files.append(font_file)
+            # Check for font file usage:
+            used_font_files.extend([font_file for font_file in font_files if font_file in file_content])
+        except Exception:
+            print(f"Problem checking file: {file_full_path}")
+            raise
 
         # Remove used fonts:
         for font_file in used_font_files:
@@ -49,8 +57,7 @@ for currentdir, subdirs, files in os.walk(rootdir):
 
 unused_font_files_count = len(font_files)
 used_font_files_count = len(used_font_files)
-print(
-    f"Font files count: {original_font_files_count}, used: {used_font_files_count}, NOT used: {unused_font_files_count}")
+print(f"Font files count: {original_font_files_count}, used: {used_font_files_count}, NOT used: {unused_font_files_count}")
 print("Remove NOT used fonts: ", font_files)
 
 for font_file in font_files:
